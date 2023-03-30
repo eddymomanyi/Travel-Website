c2923fb2bf066d8776541a3ed2651921941904dc
Eddy Momanyi <eddymomanyi@gmail.com>
2023-03-30 15:15:50 -0400

    modified HeroSection css

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -7,9 +7,9 @@ import './HeroSection.css'
 function HeroSection() {
   return (
     <div className="hero-container">
-        <video src="../videos/video-2.mp4" autoPlay loop muted/>
-        <h1>ADVENTURE AWAITS</h1>
-        <p>What are you waiting for?</p>
+        <video src="../videos/video-7.mp4" autoPlay loop muted/>
+        <h1>SEEK ADVENTURE</h1>
+        <p>DISCOVER THE THRILLS OF THE WORLD</p>
         <div className="hero-btns">
             <Button className="btns" buttonStyle='btn--outline' buttonSize='btn--large'>
                 GET STARTED
